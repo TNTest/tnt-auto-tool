1c6cf8544ad79b957b9ae80bf1c77f9966d84495
tntest <45514579@qq.com>
2013-10-31 00:35:43 +0800

    remove additinal import

--- MySelPrj/src/main/java/com/tntest/automation/FileConfiguration.java
+++ MySelPrj/src/main/java/com/tntest/automation/FileConfiguration.java
@@ -9,10 +9,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
